0bf1bb08cf42a2ffa1740cbd676710728ca7a6b6
john mathew <johnmathewloren27@gmail.com>
2025-06-29 13:50:25 +0900

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   <>
   <Routes>
     <Route path='/' element={<Home/>}/>
-    <Route path='about' element={<About/>}/>
+    <Route path='/about' element={<About/>}/>
   </Routes>
   </>
  )
